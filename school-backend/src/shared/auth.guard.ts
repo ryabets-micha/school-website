f43967927ae4553fef9e0bbd0f547db8519f100d
m.ryabets@adus-technologies.com <micha4321>
2020-05-12 02:17:06 +0200

    Added authentication

---
+++ school-backend/src/shared/auth.guard.ts
@@ -0,0 +1,29 @@
+import {Injectable, CanActivate, ExecutionContext, HttpException, HttpStatus} from '@nestjs/common';
+import * as jwt from 'jsonwebtoken';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+    async canActivate(context: ExecutionContext): Promise<boolean> {
+        const request = context.switchToHttp().getRequest();
+        if (!request.headers.authorization) return false;
+
+        request.user = await this.validateToken(request.headers.authorization);
+        // console.log(request);
+
+        return true;
+    }
+
+    async validateToken(auth: string) {
+        if (auth.split(' ')[0] !== 'Bearer') {
+            throw new HttpException('Invalid token', HttpStatus.FORBIDDEN);
+        }
+        const token = auth.split(' ')[1];
+
+        try {
+            return await jwt.verify(token, process.env.SECRET);
+        } catch (e) {
+            const message = `Token error: ${e.message || e.name}`;
+            throw new HttpException(message, HttpStatus.FORBIDDEN);
+        }
+    }
+}
